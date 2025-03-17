db13691cee4b85c5fead0e48072fb9e05a237cb3
teddyomondiii <teddyomondi@zetech.ac.ke>
2025-03-17 21:29:22 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,75 +172,75 @@ modals.forEach((modal) => { // Loops through each modal
     });
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-    const name = document.getElementById("name"); // Get the name element
-    // Create the canvas for the background
-    const canvas = document.createElement("canvas");
-    canvas.id = "backgroundCanvas";
-    document.body.appendChild(canvas); // Append the canvas to the body
+// document.addEventListener("DOMContentLoaded", function() {
+//     const name = document.getElementById("name"); // Get the name element
+//     // Create the canvas for the background
+//     const canvas = document.createElement("canvas");
+//     canvas.id = "backgroudCanvas";
+//     document.body.appendChild(canvas); // Append the canvas to the body
 
-    const ctx = canvas.getContext("2d");
+//     const ctx = canvas.getContext("2d");
 
-    // Set canvas size to cover the entire screen
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+//     // Set canvas size to cover the entire screen
+//     canvas.width = window.innerWidth;
+//     canvas.height = window.innerHeight;
 
-    const spheres = [];
-    const numSpheres = 0; // Number of spheres
+//     const spheres = [];
+//     const numSpheres = 0; // Number of spheres
 
-    // Create multiple spheres with different properties
-    for (let i = 0; i < numSpheres; i++) {
-        spheres.push({
-            angle: Math.random() * 2 * Math.PI,
-            radius: 100 + Math.random() * 50, // Randomize radius
-            centerX: Math.random() * canvas.width,
-            centerY: Math.random() * canvas.height,
-            speed: 0.01 + Math.random() * 0.02, // Randomize speed
-            originalCenterX: Math.random() * canvas.width,
-            originalCenterY: Math.random() * canvas.height
-        });
-    }
+//     // Create multiple spheres with different properties
+//     for (let i = 0; i < numSpheres; i++) {
+//         spheres.push({
+//             angle: Math.random() * 2 * Math.PI,
+//             radius: 100 + Math.random() * 50, // Randomize radius
+//             centerX: Math.random() * canvas.width,
+//             centerY: Math.random() * canvas.height,
+//             speed: 0.01 + Math.random() * 0.02, // Randomize speed
+//             originalCenterX: Math.random() * canvas.width,
+//             originalCenterY: Math.random() * canvas.height
+//         });
+//     }
 
-    function drawBackground() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+//     function drawBackground() {
+//         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Draw each sphere
-        spheres.forEach(sphere => {
-            const x = sphere.centerX + sphere.radius * Math.cos(sphere.angle);
-            const y = sphere.centerY + sphere.radius * Math.sin(sphere.angle);
+//         // Draw each sphere
+//         spheres.forEach(sphere => {
+//             const x = sphere.centerX + sphere.radius * Math.cos(sphere.angle);
+//             const y = sphere.centerY + sphere.radius * Math.sin(sphere.angle);
 
-            drawSphere(x, y);
+//             drawSphere(x, y);
 
-            sphere.angle += sphere.speed;
-            if (sphere.angle >= 2 * Math.PI) {
-                sphere.angle = 0;
-            }
-        });
+//             sphere.angle += sphere.speed;
+//             if (sphere.angle >= 2 * Math.PI) {
+//                 sphere.angle = 0;
+//             }
+//         });
 
-        requestAnimationFrame(drawBackground);
-    }
+//         requestAnimationFrame(drawBackground);
+//     }
 
-    function drawSphere(x, y) {
-        const gradient = ctx.createRadialGradient(x, y, 0, x, y, 50);
-        gradient.addColorStop(0, 'rgba(255, 255, 255, 1)');
-        gradient.addColorStop(1, 'rgba(0, 0, 255, 0)');
+//     function drawSphere(x, y) {
+//         const gradient = ctx.createRadialGradient(x, y, 0, x, y, 50);
+//         gradient.addColorStop(0, 'rgba(255, 255, 255, 1)');
+//         gradient.addColorStop(1, 'rgba(0, 0, 255, 0)');
 
-        ctx.beginPath();
-        ctx.arc(x, y, 50, 0, Math.PI * 2, false);
-        ctx.fillStyle = gradient;
-        ctx.fill();
-        ctx.closePath();
-    }
+//         ctx.beginPath();
+//         ctx.arc(x, y, 50, 0, Math.PI * 2, false);
+//         ctx.fillStyle = gradient;
+//         ctx.fill();
+//         ctx.closePath();
+//     }
 
-    drawBackground();
+//     drawBackground();
 
-    window.addEventListener('resize', () => {
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    });
+//     window.addEventListener('resize', () => {
+//         canvas.width = window.innerWidth;
+//         canvas.height = window.innerHeight;
+//     });
 
-    console.log('DOM fully loaded and parsed');
-});
+//     console.log('DOM fully loaded and parsed');
+// });
 
 function scrollLeft() {
     document.querySelector('.project-cards').scrollBy({
